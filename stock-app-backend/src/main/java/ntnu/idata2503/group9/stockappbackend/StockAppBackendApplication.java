a80177f4c5d862d41dc3831d81b066b9678a5856
Sebastiannilsen <sebnilse@gmail.com>
2023-10-19 12:09:04 +0200

    init commit

---
+++ stock-app-backend/src/main/java/ntnu/idata2503/group9/stockappbackend/StockAppBackendApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idata2503.group9.stockappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAppBackendApplication.class, args);
+	}
+
+}
